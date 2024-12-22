6d6065f9fc071ab0f058746f84066b6839c27143
klu40 <klu40@uclive.nz>
2024-12-22 15:45:53 +1300

    ai summerizer tab adjustments

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -4,7 +4,7 @@ import { Tabs } from "./ui/tabs";
 import Summerizer from "./summerizer";
 import DummyContent from "./dummy";
 import HeroSummerizer from "./heroSummerizer";
-import { BackgroundLines } from "./ui/background-lines";
+import { LinkPreview } from "./ui/link-preview";
 
 const Hero = () => {
     const tabs = [
@@ -13,7 +13,13 @@ const Hero = () => {
           value: "summerizer",
           content: (    
             <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-inter text-white bg-gradient-to-br from-blue-400 via-blue-600 to-blue-900">
-              <p className="font-extralight bg-gradient-to-r from-orange-300 via-orange-500 to-red-600 bg-clip-text text-transparent">AI Summerizer</p>
+              <div className="flex items-center">
+                <LinkPreview url="https://tailwindcss.com" className="font-extralight bg-gradient-to-l from-orange-300 via-orange-500 to-orange-600 bg-clip-text text-transparent">
+                  AI Summerizer
+                </LinkPreview>
+                <img /> {/* Palceholder img for now -- for link icon */}
+              </div>
+
               <div className="relative z-10 flex justify-start items-center w-full h-screen flex-col max-w-7xl mx-auto sm:px-16 px-6 top-16">
                 <HeroSummerizer />
                 <Summerizer />
